46faae6a2678b9638c0135b7f4126f726c725ab6
Noman <alter.noman@gmail.com>
2017-10-01 19:28:13 +0300

    Домашние задание 3, задача 4: создание одномерного массива из двумерного функцией linearize.

---
+++ HomeTask/src/DZ3/LinearizeMethod.java
@@ -0,0 +1,42 @@
+package DZ3;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class LinearizeMethod {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Задайте мерность массива m*n: ");
+        int dimensionN = in.nextInt();
+        int dimensionM = in.nextInt();
+        int[][] twoDimensionalRandomArray = formRandomArray(dimensionN, dimensionM);
+
+        System.out.println("Двумерный массив: " + Arrays.deepToString(twoDimensionalRandomArray));
+        System.out.println("Одномерный массив:" + Arrays.toString(linearize(twoDimensionalRandomArray)));
+    }
+
+    private static int[] linearize(int[][] inputArray){
+        int[] outArray = new int[inputArray.length * inputArray[0].length];
+        int outArrayIndex = 0;
+        for (int i = 0; i < inputArray.length ; i++) {
+            for (int j = 0; j < inputArray[0].length; j++) {
+                outArray[outArrayIndex] = inputArray[i][j];
+                outArrayIndex++;
+            }
+        }
+        return outArray;
+    }
+
+    private static int[][] formRandomArray(int dimensionX, int dimensionY) {
+        Random random = new Random();
+        int[][] outputArray = new int[dimensionX][dimensionY];
+        for (int i = 0; i < dimensionX; i++) {
+            for (int j = 0; j < dimensionY; j++) {
+                outputArray[i][j] = random.nextInt(10);
+            }
+       }
+        return outputArray;
+    }
+}
